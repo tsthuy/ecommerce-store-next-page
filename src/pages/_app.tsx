84a2c27d5d75dcb092883f59c45f706e59df857f
tsthuy <huydo282003@gmail.com>
2025-02-08 18:23:24 +0700

    feat: complete sign-in sign up with clerk

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,9 @@ import "~/styles/globals.css";
 import type { AppProps } from "next/app";
 
 import { Poppins } from "next/font/google";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const poppins = Poppins({
   weight: ["400", "700"],
@@ -9,10 +12,17 @@ const poppins = Poppins({
   variable: "--font-poppins",
 });
 
+const queryClient = new QueryClient();
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={poppins.className}>
-      <Component {...pageProps} />;
-    </main>
+    <ClerkProvider>
+      <QueryClientProvider client={queryClient}>
+        <main className={poppins.className}>
+          <Component {...pageProps} />
+        </main>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </ClerkProvider>
   );
 }
